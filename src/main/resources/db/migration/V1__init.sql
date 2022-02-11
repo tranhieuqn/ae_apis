32214e63a2bf604ca7c109c3548d7cceac8da8ae
Hong Kim <kimxuanhong@outlook.com>
2022-02-11 23:08:58 +0700

    Revert old flyway migration file & add new flyway migration

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -98,23 +98,3 @@ CREATE TABLE IF NOT EXISTS `order_detail` (
     FOREIGN KEY `fk__order_detail__property`(`property_id`) REFERENCES `property`(`id`)
     )
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE IF NOT EXISTS `account` (
-    `id`                 BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `phone_number`       VARCHAR(50) NULL UNIQUE,
-    `email`              VARCHAR(50) NULL UNIQUE,
-    `user_role`          VARCHAR(50) NULL,
-    `password`           VARCHAR(70) NULL,
-    `first_name`         VARCHAR(250) NULL,
-    `last_name`          VARCHAR(250) NULL,
-    `avatar_url`         VARCHAR(250) NULL,
-    `gender`             VARCHAR(50) NULL,
-    `login_status`       VARCHAR(50) NULL,
-    `register_status`    VARCHAR(50) NULL,
-    `registration_token` VARCHAR(250) NULL,
-    `address`            TEXT NULL,
-    `created_date`       TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3),
-    `modified_date`      TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3)
-    )
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
