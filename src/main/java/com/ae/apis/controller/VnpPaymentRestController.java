b38db1572f7b31ba2bca86d127d29d98f0e74c12
Hong Kim <kimxuanhong@outlook.com>
2022-02-15 22:29:56 +0700

    Add VnpPaymentRestController.java

---
+++ src/main/java/com/ae/apis/controller/VnpPaymentRestController.java
@@ -0,0 +1,38 @@
+package com.ae.apis.controller;
+
+import com.ae.apis.service.payment.VnpPaymentService;
+import com.ae.apis.service.payment.dto.CheckPaymentProcessResponse;
+import com.ae.apis.service.payment.dto.PaymentIPNProcessResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/payment/vnp")
+public class VnpPaymentRestController {
+    private static final Logger logger = LoggerFactory.getLogger(VnpPaymentRestController.class);
+
+    @Autowired
+    private VnpPaymentService vnpPaymentService;
+
+    @GetMapping("/ipn")
+    public PaymentIPNProcessResponse paymentIPNProcess(@RequestParam Map<String,String> requestParams) {
+        logger.info("PaymentController::paymentIPNProcess");
+        PaymentIPNProcessResponse paymentResponse = vnpPaymentService.paymentIPNProcess(requestParams);
+        return paymentResponse;
+    }
+
+    @GetMapping("/return")
+    public CheckPaymentProcessResponse checkPaymentProcess(@RequestParam Map<String,String> requestParams) {
+        logger.info("MobileVnpPaymentRestController::checkPaymentProcess");
+        CheckPaymentProcessResponse paymentResponse = vnpPaymentService.checkPaymentProcess(requestParams);
+        return paymentResponse;
+    }
+
+}
